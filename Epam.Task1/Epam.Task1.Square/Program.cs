d71bb096b4908eebf70ccfaed55da841a2574191
TimofeyRudometkin <45077046+TimofeyRudometkin@users.noreply.github.com>
2018-11-20 00:09:46 +0400

    Add files via upload

--- Epam.Task1/Epam.Task1.Square/Program.cs
+++ Epam.Task1/Epam.Task1.Square/Program.cs
@@ -11,7 +11,10 @@ namespace Epam.Task1.Square
         private static void Task0_3(int N)
         {
             if (N % 2 == 0)
+            {
                 Console.WriteLine("Введено чётное число, поэтому центр будет смещён вверх и влево.");
+            }
+
             for (int TheWidthOfTheSquare = 1; TheWidthOfTheSquare <= N; TheWidthOfTheSquare++)
             {
                 for (int TheHeightOfTheSquare = 1; TheHeightOfTheSquare <= N; TheHeightOfTheSquare++)
@@ -27,11 +30,34 @@ namespace Epam.Task1.Square
                 }
                 Console.WriteLine();
             }
+
+            Console.WriteLine("Введите положительное нечётное число равное длине стороны квадрата, чтобы продолжить,'0' или отрицательное число для выхода из программы");
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите положительное нечётное число равное длине стороны квадрата.");
-            Task0_3(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Введите положительное нечётное число равное длине стороны квадрата, чтобы продолжить,'0' или отрицательное число для выхода из программы");
+
+            while (true)
+            {
+                try
+                {
+                    int TheLengthOfASideOfASquare = int.Parse(Console.ReadLine());
+
+                    if (TheLengthOfASideOfASquare <= 0)
+                    {
+                        break;
+                    }
+                    Task0_3(TheLengthOfASideOfASquare);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введена строка не являющаяся положительным нечётным числом, повторите ввод положительного нечётного числа равного длине стороны квадрата, '0' или отрицательное число для выхода из программы.");
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine("Введённое число для меня крутова-то, ведите число поменьше.");
+                }
+            }
         }
     }
 }
