bc9e71f279b9de8928318cf49ae9745ce35e84f8
TimofeyRudometkin <timoha198613@gmail.com>
2018-11-21 23:56:33 +0400

    Черновые решения по Task 2

---
+++ Epam.Task2/Epam.Task2.5.SumOfNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2._5.SumOfNumbers
+{
+    class Program
+    {
+        private static void SumOfNumbers()
+        {
+            while (true)
+            {
+                int N = int.Parse(Console.ReadLine());
+
+                if(N==0)
+                {
+                    break;
+                }
+                for (int i = 1; i < N; i++)
+                {
+                    if ((i % 3 == 0) | (i % 5 == 0))
+                    {
+                        Console.Write(i+"  ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            SumOfNumbers();
+        }
+    }
+}
