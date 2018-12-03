c1c4e3d1fd3b92a3edce3cc2c19614e44bdcd470
TimofeyRudometkin <timoha198613@gmail.com>
2018-12-03 20:32:08 +0400

    fixed task 6, to adjust the task 5.

--- Epam.Task2/Epam.Task2.5.SumOfNumbers/Program.cs
+++ Epam.Task2/Epam.Task2.5.SumOfNumbers/Program.cs
@@ -8,15 +8,15 @@ namespace Epam.Task2._5.SumOfNumbers
         {
                 int Sum = 0;
                 
-                for (int i = 1; i < 1000; i++)
+                for (int i = 3; i < 1000; i++)
                 {
-                    if ((i % 3 == 0) | (i % 5 == 0))
+                    if ((i % 3 == 0) || (i % 5 == 0))
                     {
                         Sum +=i;
                     }
                 }
 
-                Console.WriteLine("Sum of all multiples of 3 or 5 and less than 1000 = " + Sum);
+                Console.WriteLine($"Sum of all multiples of 3 or 5 and less than 1000 = {Sum}" );
             }
         static void Main(string[] args)
         {
--- Epam.Task2/Epam.Task2.6.FontAdjustment/Program.cs
+++ Epam.Task2/Epam.Task2.6.FontAdjustment/Program.cs
@@ -9,33 +9,67 @@ namespace Epam.Task2._6.FontAdjustment
     class Program
     {
         [Flags]
-        private enum enFontAdjustment { Bold, Italic, Underline }
+        private enum enFont { None=0, Bold=1, Italic= 2, Underline=4 }
         private static void FontAdjustment()
         {
             Console.WriteLine("Label options: None");
 
             int tracing;
+            enFont Font = enFont.None;
 
             while (true)
             { 
                 Console.WriteLine("Enter:"+ System.Environment.NewLine+"        1: bold" + System.Environment.NewLine + 
                     "        2: italic" + System.Environment.NewLine + "        3: underline" + 
-                    System.Environment.NewLine + "        4: italic, underline"+ System.Environment.NewLine + 
-                    "        0: for exit from this program.");
+                    System.Environment.NewLine + "        0: for exit from this program.");
 
                 try
                 {
-                    tracing = int.Parse(Console.ReadLine()) - 1;
-                    if (tracing == -1)
+                    tracing = int.Parse(Console.ReadLine());
+                    if (tracing==0)
                     {
                         break;
+                    }
+                    switch (tracing)
+                    {
+                        case 1:
+                            if ((Font & enFont.Bold) == enFont.Bold)
+                            {
+                                Font = Font ^ enFont.Bold;
+                            }
+                            else
+                            {
+                                Font = Font | enFont.Bold;
+                            }
+                            break;
+                        case 2:
+                            if ((Font & enFont.Italic) == enFont.Italic)
+                            {
+                                Font = Font ^ enFont.Italic;
+                            }
+                            else
+                            {
+                                Font = Font | enFont.Italic;
+                            }
+                            break;
+                        case 3:
+                            if ((Font & enFont.Underline) == enFont.Underline)
+                            {
+                                Font = Font ^ enFont.Underline;
+                            }
+                            else
+                            {
+                                Font = Font | enFont.Underline;
+                            }
+                            break;
                     }
                 }
                 catch (FormatException)
                 {
+                    Console.WriteLine("Label options: {0}", Font);
                     continue;
                 }
-                Console.WriteLine("Label options: " + (enFontAdjustment)tracing);
+                Console.WriteLine("Label options: {0}", Font);
             }
         }
         static void Main(string[] args)
